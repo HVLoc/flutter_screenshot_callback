5ba722e2e74414eda01f3b709e1e7a19e1119762
lochv <lochv@intern.softdreams.vn>
2022-03-17 10:46:48 +0700

    update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:screenshot_callback/screenshot_callback.dart';
+import 'package:screenshot_callback_fork/screenshot_callback.dart';
 
 void main() => runApp(MyApp());
 
